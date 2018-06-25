5710bdcfbf90b90573c43cd36d5db81631e2efee
Matthew <mdwareing@gmail.com>
2018-06-25 15:29:54 +0100

    Use module examples

---
+++ exclaim-test.js
@@ -0,0 +1,8 @@
+var repeat = require("./repeat").repeat;
+var exclaim = require("./exclaim").exclaim;
+
+if(exlaim(repeat, "hi") !== "hi!!!!!") {
+    throw new Error("Exclaiming hi should be equal hi!!!!!");
+} else {
+    console.log(".");
+}
